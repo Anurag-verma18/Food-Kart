64ce6cf596dff86288dd087977fae22630149675
Anurag-verma18 <anuragkrverma18@gmail.com>
2024-03-13 18:21:02 +0530

    feat: add scrolling feature to bannerlist and top restaurants

--- src/components/BannerList.js
+++ src/components/BannerList.js
@@ -1,6 +1,33 @@
-
+import {useEffect, useRef, useState} from "react";
+import { GoArrowLeft } from "react-icons/go";
+import { GoArrowRight } from "react-icons/go";
 
 const BannerList = ({listOfBanner}) => {
+  const bannerRef = useRef(null);
+  const [scrollVal, setScrollVal] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (bannerRef.current) {
+        setScrollVal(bannerRef.current.scrollLeft);
+      }
+    };
+    if (bannerRef.current) {
+      bannerRef.current.addEventListener('scroll', handleScroll);
+    }
+    return () => {
+      if (bannerRef.current) {
+        bannerRef.current.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
+  
+  const handleBannerScroll = (step) => {
+    if(bannerRef.current) {
+      bannerRef.current.scrollLeft += step;
+    }
+  };
+
     return (
         <div className="w-full mx-auto">
             <div className="relative">
@@ -8,8 +35,22 @@ const BannerList = ({listOfBanner}) => {
                 <div className="flex justify-start mb-3 pl-3">
                   <div className="text-lg font-semibold tracking-wide">What's on your mind?</div>
                 </div>
-                <div className="banner-list flex overflow-x-scroll no-scrollbar">
-                  <div className="flex items-center">
+                <div className="absolute top-0 flex gap-2 right-8">
+                  <button onClick={() => handleBannerScroll(-400)} 
+                          disabled={scrollVal===0}
+                          className={`${scrollVal===0 ? "text-slate-400" : "text-black"} flex justify-center items-center 
+                          cursor-pointer rounded-full w-6 h-6 p-1 bg-slate-200 hover:bg-slate-300`}>
+                          
+                        <GoArrowLeft />
+                  </button>
+                  <button onClick={() => handleBannerScroll(400)} 
+                          className="text-black flex justify-center items-center cursor-pointer rounded-full w-6 h-6 p-1 
+                          bg-slate-200 hover:bg-slate-300">
+                        <GoArrowRight />
+                  </button>
+                </div>
+                <div className="flex overflow-x-auto scroll-smooth no-scrollbar" ref={bannerRef}>
+                  <div className="flex items-center" >
                     {listOfBanner?.map((banner) => (
                       <div className="pr-5 w-36 hover:scale-95 transition ease-in-out rounded-lg" 
                            key={banner?.id}>
--- src/components/TopRestaurants.js
+++ src/components/TopRestaurants.js
@@ -1,8 +1,37 @@
 import RestaurantCard from "./RestaurantCard";
 import { Link } from "react-router-dom";
+import {useEffect, useRef, useState} from "react";
+import { GoArrowLeft } from "react-icons/go";
+import { GoArrowRight } from "react-icons/go";
 
 
 const TopRestaurants = ({listOfTopRestaurants}) => {
+
+  const restRef = useRef(null);
+  const [scrollVal, setScrollVal] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (restRef.current) {
+        setScrollVal(restRef.current.scrollLeft);
+      }
+    };
+    if (restRef.current) {
+      restRef.current.addEventListener('scroll', handleScroll);
+    }
+    return () => {
+      if (restRef.current) {
+        restRef.current.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
+  
+  const handleTopRestScroll = (step) => {
+    if(restRef.current) {
+      restRef.current.scrollLeft += step;
+    }
+  };
+
     return (
         <div className="w-full mx-auto">
             <div className="relative">
@@ -10,7 +39,21 @@ const TopRestaurants = ({listOfTopRestaurants}) => {
                 <div className="flex justify-start mb-2 pl-3">
                   <div className="text-lg font-semibold tracking-wide">Top restaurant chains in Bangalore</div>
                 </div>
-                <div className="top-rest-list flex overflow-x-scroll no-scrollbar">
+                <div className="absolute top-0 flex gap-2 right-8">
+                  <button onClick={() => handleTopRestScroll(-400)} 
+                          disabled={scrollVal===0}
+                          className={`${scrollVal===0 ? "text-slate-400" : "text-black"} flex justify-center items-center 
+                          cursor-pointer rounded-full w-6 h-6 p-1 bg-slate-200 hover:bg-slate-300`}>
+                          
+                        <GoArrowLeft />
+                  </button>
+                  <button onClick={() => handleTopRestScroll(400)} 
+                          className="text-black flex justify-center items-center cursor-pointer rounded-full w-6 h-6 p-1 
+                          bg-slate-200 hover:bg-slate-300">
+                        <GoArrowRight />
+                  </button>
+                </div>
+                <div className="top-rest-list flex overflow-x-scroll scroll-smooth no-scrollbar" ref={restRef}>
                   <div className="flex">
                     {
                       listOfTopRestaurants.map((restaurant) => (
