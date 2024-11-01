1d5ecf187e561253006ec9ab8c78b52192646493
Anurag-verma18 <anuragkrverma18@gmail.com>
2024-11-01 12:03:31 +0530

    fetch menu details without any delay

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -5,16 +5,12 @@ const useRestaurantMenu = (resId) => {
     const [resInfo, setResInfo] = useState(null);
 
     useEffect(() => {
-        setTimeout(() => {
-            fetchMenu();
-        }, 10000)
-        //fetchMenu();
+        fetchMenu();
     }, []);
 
     const fetchMenu = async () => {
         const data = await fetch( MENU_API + resId );
         const json = await data.json(); 
-        console.log(json);
         setResInfo(json.data);
     };
 
